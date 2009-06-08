e51e00eb0f02eee7f94ae2ec4c489cb25164119a
Francois BERENGER <francois.berenger.working@gmail.com>
2009-06-08 22:31:19 +0900

    * the parser is no more trapped by string consts in comments or code * various renamings to help code understanding

--- CommentRemoval.hs
+++ CommentRemoval.hs
@@ -19,51 +19,68 @@ type LongCmtStr  = String
 -- A source file can be seen as a list of text lines. Each one
 -- being made of different items :
 -- source code, comments, comment delimiters, etc.
-data LowLevelToken = CmtOrCode    Char
-                   | CmtBegin Token
-                   | CmtEnd   Token
-                   | LineCmtMark  Token
---                    | StringBegin  Token
---                    | StringEnd    Token
+data LowLevelToken = CmtOrCodeOrString Char
+                   | CmtBegin          Token
+                   | CmtEnd            Token
+                   | LineCmtMark       Token
+                   | StringBeginOrEnd  Token
                    deriving Show
+-- @TODO FBR: to not trap the parser with "fake" tokens embedded in
+--            String consts, handle these soon:
 
 data HighLevelToken = Code     CodeStr
                     | ShortCmt ShortCmtStr
                     | LongCmt  LongCmtStr
+                    | StringInCode     CodeStr
+                    | StringInShortCmt ShortCmtStr
+                    | StringInLongCmt  LongCmtStr
                     deriving Show
 
 data ParserState = ReadingCode
                  | ReadingShortCmt
                  | ReadingLongCmt
--- @TODO FBR: to not trap the parser with "fake" tokens embedded in
---            String consts, handle these soon:
--- @TODO: FBR: write a unix CLI tool removing comments language independently
---        1) tokenize in {code | short comment | long comments}
---        1') escape string consts from the parser
---        2) remove comments
---        2') tokenize code in {const | keyword | spacing}
+                 | ReadingStringInCode
+                 | ReadingStringInShortCmt
+                 | ReadingStringInLongCmt
+-- @TODO: 1) tokenize in {code | short comment | long comments},
+--           embedding string consts
+--        2) tokenize code in {const | keyword | spacing}
 --        3) extract inter line indentation rules
 --        3') apply indentation rules
 --        4) extract intra line indentation rules
 
 lltToString :: LowLevelToken -> String
-lltToString (CmtOrCode    s) = s:[]
-lltToString (CmtBegin s) = s
-lltToString (CmtEnd   s) = s
-lltToString (LineCmtMark  s) = s
+lltToString (CmtOrCodeOrString s) = s:[]
+lltToString (CmtBegin          s) = s
+lltToString (CmtEnd            s) = s
+lltToString (LineCmtMark       s) = s
+lltToString (StringBeginOrEnd  s) = s
 
 hltToString :: HighLevelToken -> String
 hltToString (Code         s) = s
 hltToString (ShortCmt     s) = s
 hltToString (LongCmt      s) = s
+hltToString (StringInCode     s) = s
+hltToString (StringInShortCmt s) = s
+hltToString (StringInLongCmt  s) = s
 
 promote :: ParserState -> LowLevelToken -> HighLevelToken
-promote ReadingCode     (CmtOrCode    x) = Code (x:[])
-promote ReadingShortCmt (CmtOrCode    x) = ShortCmt (x:[])
-promote ReadingLongCmt  (CmtOrCode    x) = LongCmt (x:[])
-promote _ (CmtBegin x) = LongCmt  x
-promote _ (CmtEnd   x) = LongCmt  x
-promote _ (LineCmtMark  x) = ShortCmt x
+promote ReadingCode             (CmtOrCodeOrString x) = Code             (x:[])
+promote ReadingShortCmt         (CmtOrCodeOrString x) = ShortCmt         (x:[])
+promote ReadingLongCmt          (CmtOrCodeOrString x) = LongCmt          (x:[])
+promote ReadingStringInCode     (CmtOrCodeOrString x) = StringInCode     (x:[])
+promote ReadingStringInShortCmt (CmtOrCodeOrString x) = StringInShortCmt (x:[])
+promote ReadingStringInLongCmt  (CmtOrCodeOrString x) = StringInLongCmt  (x:[])
+promote ReadingCode             (StringBeginOrEnd x)  = StringInCode     x
+promote ReadingShortCmt         (StringBeginOrEnd x)  = StringInShortCmt x
+promote ReadingLongCmt          (StringBeginOrEnd x)  = StringInLongCmt  x
+promote ReadingStringInCode     (StringBeginOrEnd x)  = StringInCode     x
+promote ReadingStringInShortCmt (StringBeginOrEnd x)  = StringInShortCmt x
+promote ReadingStringInLongCmt  (StringBeginOrEnd x)  = StringInLongCmt  x
+promote _                       (CmtBegin    x)       = LongCmt          x
+promote _                       (CmtEnd      x)       = LongCmt          x
+promote ReadingStringInCode     (LineCmtMark x)       = StringInCode     x
+promote _                       (LineCmtMark x)       = ShortCmt         x
 
 startWithList :: [Token] -> String -> Maybe Token
 -- FBR: use a map here instead of explicit recursion ?
@@ -73,39 +90,48 @@ startWithList (prfx:others) str
     | otherwise           = startWithList others str
 
 -- the remaining string is modified by a token or by a letter consumption
-firstMatchedTok :: String -> [Token] -> [Token] -> [Token]
+firstMatchedTok :: String
+                -> [Token] -> [Token] -> [Token] -> [Token]
                 -> Maybe (LowLevelToken, String)
-firstMatchedTok [] _ _ _ = Nothing
-firstMatchedTok str@(c:cs) cmtStartList cmtEndList lineCmtList =
-    case startWithList cmtStartList str of
+firstMatchedTok [] _ _ _ _ = Nothing
+firstMatchedTok str@(c:cs) cmtStarters cmtStopers shortCmtStarters
+                stringDelims =
+    case startWithList stringDelims str of
       Just matched ->
-          Just (CmtBegin matched
+          Just (StringBeginOrEnd matched
                ,consumeTokenUnsafe matched str)
       Nothing ->
-          case startWithList cmtEndList str of
+          case startWithList cmtStarters str of
             Just matched ->
-                Just (CmtEnd matched
+                Just (CmtBegin matched
                      ,consumeTokenUnsafe matched str)
             Nothing ->
-                case startWithList lineCmtList str of
+                case startWithList cmtStopers str of
                   Just matched ->
-                      Just (LineCmtMark matched
+                      Just (CmtEnd matched
                            ,consumeTokenUnsafe matched str)
-                  Nothing -> Just (CmtOrCode c, cs)
+                  Nothing ->
+                      case startWithList shortCmtStarters str of
+                        Just matched ->
+                            Just (LineCmtMark matched
+                                 ,consumeTokenUnsafe matched str)
+                        Nothing -> Just (CmtOrCodeOrString c, cs)
     where
       -- !!! tok _MUST_ be a prefix of str !!!
       consumeTokenUnsafe :: Token -> String -> String
       consumeTokenUnsafe tok tokPrefixedStr = drop (length tok) tokPrefixedStr
 
 -- line to list of tokens
-tokenizeSrcLine :: [Token] -> [Token] -> [Token]
+tokenizeSrcLine :: [Token] -> [Token] -> [Token] -> [Token]
                 -> [LowLevelToken] -> String -> [LowLevelToken]
-tokenizeSrcLine cmtStartList cmtEndList lineCmtList acc srcLine =
-    case firstMatchedTok srcLine cmtStartList cmtEndList lineCmtList of
+tokenizeSrcLine cmtStarters cmtStopers shortCmtStarters stringDelims
+                acc srcLine =
+    case firstMatchedTok srcLine cmtStarters cmtStopers shortCmtStarters
+                         stringDelims of
       Nothing -> reverse acc
       Just (tok, remaining) -> tokenizeSrcLine
-                                 cmtStartList cmtEndList lineCmtList
-                                 (tok:acc) remaining
+                                 cmtStarters cmtStopers shortCmtStarters
+                                 stringDelims (tok:acc) remaining
 
 -- file name to list of lines read
 getLines :: String -> IO [String]
@@ -126,11 +152,12 @@ getLines fileName =
                Right line -> getLines' h (line:acc)
 
 -- file name to list of low level tokens
-tokenizeFile :: String -> [Token] -> [Token] -> [Token]
+tokenizeFile :: String -> [Token] -> [Token] -> [Token] -> [Token]
              -> IO [[LowLevelToken]]
-tokenizeFile fileName cmtStartList cmtEndList lineCmtList =
+tokenizeFile fileName cmtStarters cmtStopers shortCmtStarters stringDelims =
     do readLines <- getLines fileName
-       return (map (tokenizeSrcLine cmtStartList cmtEndList lineCmtList [])
+       return (map (tokenizeSrcLine cmtStarters cmtStopers shortCmtStarters
+                                    stringDelims [])
                    readLines)
 
 -- low to high level tokens
@@ -153,67 +180,125 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
           case parserState of
             ReadingCode ->
                 case tok of
-                  CmtOrCode _ ->
-                      highLevelTokens (toks:others) parserState depth
-                                      (factorize parserState tok lineAcc) acc
+                  CmtOrCodeOrString _ ->
+                      highLevelTokens
+                         (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
                   LineCmtMark _ ->
-                      highLevelTokens (toks:others) ReadingShortCmt depth
-                                      (factorize parserState tok lineAcc) acc
+                      highLevelTokens
+                         (toks:others) ReadingShortCmt
+                         depth (factorize parserState tok lineAcc) acc
                   CmtBegin _ ->
-                      highLevelTokens (toks:others) ReadingLongCmt (depth+1)
-                                      (factorize parserState tok lineAcc) acc
+                      highLevelTokens
+                         (toks:others) ReadingLongCmt
+                         (depth+1) (factorize parserState tok lineAcc) acc
                   CmtEnd mark ->
                       error ("got EndCmtMArk while ReadingCode: " ++ mark)
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInCode
+                         depth (factorize parserState tok lineAcc) acc
             ReadingShortCmt ->
-                highLevelTokens (toks:others) ReadingShortCmt depth
-                                (factorize parserState tok lineAcc) acc
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInShortCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  _ -> highLevelTokens
+                         (toks:others) ReadingShortCmt
+                         depth (factorize parserState tok lineAcc) acc
             ReadingLongCmt ->
                 case tok of
                   CmtEnd _ ->
                       let newDepth = depth - 1 in
                       if newDepth == 0 then
-                          highLevelTokens (toks:others) ReadingCode newDepth
-                                          (factorize parserState tok lineAcc)
-                                          acc
+                          highLevelTokens
+                             (toks:others) ReadingCode
+                             newDepth (factorize parserState tok lineAcc) acc
                       else
                           highLevelTokens
-                            (toks:others) parserState newDepth
-                            (factorize parserState tok lineAcc) acc
+                            (toks:others) parserState
+                            newDepth (factorize parserState tok lineAcc) acc
                   CmtBegin _ ->
                       let newDepth = depth + 1 in
-                      highLevelTokens (toks:others) parserState newDepth
-                                      (factorize parserState tok lineAcc) acc
-                  CmtOrCode _ ->
-                      highLevelTokens (toks:others) parserState depth
-                                      (factorize parserState tok lineAcc) acc
+                      highLevelTokens
+                         (toks:others) parserState
+                         newDepth (factorize parserState tok lineAcc) acc
+                  CmtOrCodeOrString _ ->
+                      highLevelTokens
+                         (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
                   LineCmtMark mark ->
                       error ("got LineCmtMark while ReadingLongCmt: " ++ mark)
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInLongCmt
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingStringInLongCmt ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingLongCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingStringInShortCmt ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingShortCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingStringInCode ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingCode
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
 
 factorize :: ParserState -> LowLevelToken -> [HighLevelToken]
           -> [HighLevelToken]
 factorize state elt [] = (promote state elt):[]
 factorize state elt lst@(x:xs) =
     case elt of
-      CmtOrCode _ ->
+      CmtOrCodeOrString _ ->
           case x of
-            Code code -> (Code (code ++ (lltToString elt))):xs
-            _         -> (promote state elt):lst
+            Code c -> (Code (c ++ (lltToString elt))):xs
+            _      -> (promote state elt):lst
       CmtBegin _ ->
           case x of
-            LongCmt longCmt -> (LongCmt (longCmt ++ (lltToString elt))):xs
-            _               -> (promote state elt):lst
+            LongCmt l -> (LongCmt (l ++ (lltToString elt))):xs
+            _         -> (promote state elt):lst
       CmtEnd _ ->
           case x of
-            LongCmt longCmt -> (LongCmt (longCmt ++ (lltToString elt))):xs
-            _               -> (promote state elt):lst
+            LongCmt l -> (LongCmt (l ++ (lltToString elt))):xs
+            _         -> (promote state elt):lst
       LineCmtMark _ ->
           case x of
-            ShortCmt shortCmt -> (ShortCmt (shortCmt ++ (lltToString elt))):xs
-            _                 -> (promote state elt):lst
+            ShortCmt s -> (ShortCmt (s ++ (lltToString elt))):xs
+            _          -> (promote state elt):lst
+      StringBeginOrEnd _ ->
+          case x of
+            StringInCode     s -> (StringInCode
+                                   (s ++ (lltToString elt))):xs
+            StringInShortCmt s -> (StringInShortCmt
+                                   (s ++ (lltToString elt))):xs
+            StringInLongCmt  s -> (StringInLongCmt
+                                   (s ++ (lltToString elt))):xs
+            _ -> (promote state elt):lst
 
 rmCmtsWrapper :: String -> IO [[HighLevelToken]]
 rmCmtsWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
        let highLevelToks = highLevelTokens lowLevelToks ReadingCode 0 [] []
            codeOnly = removeComments highLevelToks
        return codeOnly
@@ -222,16 +307,17 @@ rmCmtsWrapper fileName =
       removeComments srcLine = map (filter isCode) srcLine
 
       isCode :: HighLevelToken -> Bool
-      isCode (Code _) = True
-      isCode _        = False
+      isCode (Code _)         = True
+      isCode (StringInCode _) = True
+      isCode _                = False
 
 lowLevelTokenizeWrapper :: String -> IO [[LowLevelToken]]
 lowLevelTokenizeWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
        return lowLevelToks
 
 highLevelTokenizeWrapper :: String -> IO [[HighLevelToken]]
 highLevelTokenizeWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
        let highLevelToks = highLevelTokens lowLevelToks ReadingCode 0 [] []
        return highLevelToks
