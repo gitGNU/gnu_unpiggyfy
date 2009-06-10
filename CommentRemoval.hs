bf4444300bb7d1cd2813cbe886e8788b027ee0b3
Francois BERENGER <francois.berenger.working@gmail.com>
2009-06-10 22:12:46 +0900

    manage escape chars in strings

--- CommentRemoval.hs
+++ CommentRemoval.hs
@@ -1,12 +1,14 @@
 module CommentRemoval (
     HighLevelToken,
     hltToString,
+    isOnlyIndentationLine,
     rmCmtsWrapper,
     lowLevelTokenizeWrapper,
     highLevelTokenizeWrapper,
 ) where
 
 import Control.Exception
+import Data.Char (isSpace)
 import Data.Maybe
 import Data.List
 import System.IO
@@ -18,12 +20,13 @@ type LongCmtStr  = String
 
 -- A source file can be seen as a list of text lines. Each one
 -- being made of different items :
--- source code, comments, comment delimiters, etc.
+-- source code, comments, comment delimiters, string constant, etc.
 data LowLevelToken = CmtOrCodeOrString Token
                    | CmtBegin          Token
                    | CmtEnd            Token
                    | LineCmtMark       Token
                    | StringBeginOrEnd  Token
+                   | EscapedChar       Token
                    deriving Show
 
 data HighLevelToken = Code     CodeStr
@@ -40,21 +43,35 @@ data ParserState = ReadingCode
                  | ReadingStringInCode
                  | ReadingStringInShortCmt
                  | ReadingStringInLongCmt
+                 | ReadingEscCharInStringInCode
+                 | ReadingEscCharInStringInShortCmt
+                 | ReadingEscCharInStringInLongCmt
+                 deriving Show
 
--- lltToString :: LowLevelToken -> String
--- lltToString (CmtOrCodeOrString s) = s:[]
--- lltToString (CmtBegin          s) = s
--- lltToString (CmtEnd            s) = s
--- lltToString (LineCmtMark       s) = s
--- lltToString (StringBeginOrEnd  s) = s
+lltToString :: LowLevelToken -> String
+lltToString (CmtOrCodeOrString s) = s
+lltToString (CmtBegin          s) = s
+lltToString (CmtEnd            s) = s
+lltToString (LineCmtMark       s) = s
+lltToString (StringBeginOrEnd  s) = s
+lltToString (EscapedChar       s) = s
 
 hltToString :: HighLevelToken -> String
-hltToString (Code         s) = s
-hltToString (ShortCmt     s) = s
-hltToString (LongCmt      s) = s
+hltToString (Code             s) = s
+hltToString (ShortCmt         s) = s
+hltToString (LongCmt          s) = s
 hltToString (StringInCode     s) = s
 hltToString (StringInShortCmt s) = s
 hltToString (StringInLongCmt  s) = s
+
+isOnlyIndentationLine :: [HighLevelToken] -> Bool
+isOnlyIndentationLine [] = False
+isOnlyIndentationLine (x:[]) = isOnlyIndentation x
+    where
+      isOnlyIndentation (Code "") = False
+      isOnlyIndentation (Code str) = all isSpace str
+      isOnlyIndentation _ = False
+isOnlyIndentationLine _ = False
 
 promote :: ParserState -> LowLevelToken -> HighLevelToken
 promote ReadingCode             (CmtOrCodeOrString x) = Code             x
@@ -63,16 +80,31 @@ promote ReadingLongCmt          (CmtOrCodeOrString x) = LongCmt          x
 promote ReadingStringInCode     (CmtOrCodeOrString x) = StringInCode     x
 promote ReadingStringInShortCmt (CmtOrCodeOrString x) = StringInShortCmt x
 promote ReadingStringInLongCmt  (CmtOrCodeOrString x) = StringInLongCmt  x
-promote ReadingCode             (StringBeginOrEnd x)  = StringInCode     x
-promote ReadingShortCmt         (StringBeginOrEnd x)  = StringInShortCmt x
-promote ReadingLongCmt          (StringBeginOrEnd x)  = StringInLongCmt  x
-promote ReadingStringInCode     (StringBeginOrEnd x)  = StringInCode     x
-promote ReadingStringInShortCmt (StringBeginOrEnd x)  = StringInShortCmt x
-promote ReadingStringInLongCmt  (StringBeginOrEnd x)  = StringInLongCmt  x
-promote _                       (CmtBegin    x)       = LongCmt          x
-promote _                       (CmtEnd      x)       = LongCmt          x
-promote ReadingStringInCode     (LineCmtMark x)       = StringInCode     x
-promote _                       (LineCmtMark x)       = ShortCmt         x
+promote ReadingCode             (StringBeginOrEnd  x) = StringInCode     x
+promote ReadingShortCmt         (StringBeginOrEnd  x) = StringInShortCmt x
+promote ReadingLongCmt          (StringBeginOrEnd  x) = StringInLongCmt  x
+promote ReadingStringInCode     (StringBeginOrEnd  x) = StringInCode     x
+promote ReadingStringInShortCmt (StringBeginOrEnd  x) = StringInShortCmt x
+promote ReadingStringInLongCmt  (StringBeginOrEnd  x) = StringInLongCmt  x
+promote ReadingStringInCode     (EscapedChar       x) = StringInCode     x
+promote ReadingStringInShortCmt (EscapedChar       x) = StringInShortCmt x
+promote ReadingStringInLongCmt  (EscapedChar       x) = StringInLongCmt  x
+promote ReadingCode             (EscapedChar       x) =
+    error ("while ReadingCode, got EscapedChar: " ++ x)
+promote ReadingShortCmt         (EscapedChar       x) =
+    error ("while ReadingShortCmt, got EscapedChar: " ++ x)
+promote ReadingLongCmt          (EscapedChar       x) =
+    error ("while ReadingLongCmt, got EscapedChar: " ++ x)
+promote ReadingEscCharInStringInCode               x  = StringInCode
+                                                          (lltToString x)
+promote ReadingEscCharInStringInShortCmt           x  = StringInShortCmt
+                                                          (lltToString x)
+promote ReadingEscCharInStringInLongCmt            x  = StringInLongCmt
+                                                          (lltToString x)
+promote _                       (CmtBegin          x) = LongCmt          x
+promote _                       (CmtEnd            x) = LongCmt          x
+promote ReadingStringInCode     (LineCmtMark       x) = StringInCode     x
+promote _                       (LineCmtMark       x) = ShortCmt         x
 
 startWithList :: [Token] -> String -> Maybe Token
 -- FBR: use a map/fold here instead of explicit recursion ?
@@ -83,64 +115,95 @@ startWithList (prfx:others) str
 
 -- the remaining string is modified by a token or by a letter consumption
 firstMatchedTok :: String
-                -> [Token] -> [Token] -> [Token] -> [Token]
+                -> [Token] -> [Token] -> [Token] -> [Token] -> [Token]
                 -> Maybe (LowLevelToken, String)
-firstMatchedTok [] _ _ _ _ = Nothing
+firstMatchedTok [] _ _ _ _ _ = Nothing
 firstMatchedTok str@(c:cs) cmtStarters cmtStopers shortCmtStarters
-                stringDelims =
-    case startWithList stringDelims str of
+                stringDelims escChars =
+    case startWithList escChars str of
       Just matched ->
-          Just (StringBeginOrEnd matched
+          Just (EscapedChar matched
                ,consumeTokenUnsafe matched str)
       Nothing ->
-          case startWithList cmtStarters str of
+          case startWithList stringDelims str of
             Just matched ->
-                Just (CmtBegin matched
+                Just (StringBeginOrEnd matched
                      ,consumeTokenUnsafe matched str)
             Nothing ->
-                case startWithList cmtStopers str of
+                case startWithList cmtStarters str of
                   Just matched ->
-                      Just (CmtEnd matched
+                      Just (CmtBegin matched
                            ,consumeTokenUnsafe matched str)
                   Nothing ->
-                      case startWithList shortCmtStarters str of
+                      case startWithList cmtStopers str of
                         Just matched ->
-                            Just (LineCmtMark matched
+                            Just (CmtEnd matched
                                  ,consumeTokenUnsafe matched str)
-                        Nothing -> Just (CmtOrCodeOrString (c:[])
-                                        ,cs)
+                        Nothing ->
+                            case startWithList shortCmtStarters str of
+                              Just matched ->
+                                  Just (LineCmtMark matched
+                                       ,consumeTokenUnsafe matched str)
+                              Nothing -> Just (CmtOrCodeOrString (c:[])
+                                              ,cs)
     where
       -- !!! tok _MUST_ be a prefix of str !!!
       consumeTokenUnsafe :: Token -> String -> String
       consumeTokenUnsafe tok tokPrefixedStr = drop (length tok) tokPrefixedStr
 
 -- line to list of tokens
-tokenizeSrcLine :: [Token] -> [Token] -> [Token] -> [Token]
+tokenizeSrcLine :: Bool
+                -> [Token] -> [Token] -> [Token] -> [Token] -> [Token]
                 -> [LowLevelToken] -> String -> [LowLevelToken]
-tokenizeSrcLine cmtStarters cmtStopers shortCmtStarters stringDelims
+-- empty line in source file
+tokenizeSrcLine _ _ _ _ _ _ _ "" = (CmtOrCodeOrString ""):[]
+-- non empty line
+tokenizeSrcLine escaping
+                cmtStarters cmtStopers shortCmtStarters stringDelims escChars
                 acc srcLine =
     case firstMatchedTok srcLine cmtStarters cmtStopers shortCmtStarters
-                         stringDelims of
+                         stringDelims escChars of
       Nothing -> reverse acc
-      Just (tok, remaining) -> tokenizeSrcLine
-                                 cmtStarters cmtStopers shortCmtStarters
-                                 stringDelims (factorize tok acc) remaining
+      Just (tok, []) -> reverse (factorize escaping tok acc)
+      Just (tok, remaining@(_:_)) ->
+          case tok of
+            EscapedChar _ ->
+                tokenizeSrcLine (xor True escaping)
+                                cmtStarters cmtStopers shortCmtStarters
+                                stringDelims escChars
+                                (factorize escaping tok acc) remaining
+            _ ->
+                tokenizeSrcLine False
+                                cmtStarters cmtStopers shortCmtStarters
+                                stringDelims escChars
+                                (factorize escaping tok acc) remaining
     where
-      factorize :: LowLevelToken -> [LowLevelToken] -> [LowLevelToken]
-      factorize elt [] = elt:[]
-      factorize elt lst@(x:xs) =
-        case (elt, x) of
-          (CmtOrCodeOrString y, CmtOrCodeOrString z) ->
-              (CmtOrCodeOrString (z ++ y)):xs
-          (CmtBegin y, CmtBegin z) ->
-              (CmtBegin (z ++ y)):xs
-          (CmtEnd y, CmtEnd z) ->
-              (CmtEnd (z ++ y)):xs
-          (LineCmtMark y, LineCmtMark z) ->
-              (LineCmtMark (z ++ y)):xs
-          (StringBeginOrEnd y, StringBeginOrEnd z) ->
-              (StringBeginOrEnd (z ++ y)):xs
-          _ -> elt:lst
+      factorize :: Bool -> LowLevelToken -> [LowLevelToken] -> [LowLevelToken]
+      factorize _ elt [] = elt:[]
+      factorize escaping' elt lst@(x:xs)
+          | escaping' =
+              case x of
+                (EscapedChar z) ->
+                    (EscapedChar (z ++ (lltToString elt))):xs
+                _ -> error ("x is not an EscapedChar while we are escaping: "
+                            ++ (lltToString elt))
+          | otherwise =
+              case (elt, x) of
+                (CmtOrCodeOrString y, CmtOrCodeOrString z) ->
+                    (CmtOrCodeOrString (z ++ y)):xs
+                (CmtBegin y, CmtBegin z) ->
+                    (CmtBegin (z ++ y)):xs
+                (CmtEnd y, CmtEnd z) ->
+                    (CmtEnd (z ++ y)):xs
+                (LineCmtMark y, LineCmtMark z) ->
+                    (LineCmtMark (z ++ y)):xs
+                -- consecutive StringBeginOrEnd low level tokens must not be
+                -- factorized together to manage correctly the empty string ""
+                _ -> elt:lst
+
+xor :: Bool -> Bool -> Bool
+xor True True = False
+xor _ _ = True
 
 -- file name to list of lines read
 getLines :: String -> IO [String]
@@ -160,12 +223,14 @@ getLines fileName =
                Right line -> getLines' h (line:acc)
 
 -- file name to list of low level tokens
-tokenizeFile :: String -> [Token] -> [Token] -> [Token] -> [Token]
+tokenizeFile :: String -> [Token] -> [Token] -> [Token] -> [Token] -> [Token]
              -> IO [[LowLevelToken]]
-tokenizeFile fileName cmtStarters cmtStopers shortCmtStarters stringDelims =
+tokenizeFile fileName cmtStarters cmtStopers shortCmtStarters stringDelims
+             escChars =
     do readLines <- getLines fileName
-       return (map (tokenizeSrcLine cmtStarters cmtStopers shortCmtStarters
-                                    stringDelims [])
+       return (map (tokenizeSrcLine False
+                                    cmtStarters cmtStopers shortCmtStarters
+                                    stringDelims escChars [])
                    readLines)
 
 -- low to high level tokens
@@ -201,18 +266,22 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
                       highLevelTokens
                          (toks:others) ReadingLongCmt
                          (depth+1) (factorize parserState tok lineAcc) acc
-                  CmtEnd mark ->
-                      error ("got EndCmtMArk while ReadingCode: " ++ mark)
                   StringBeginOrEnd _ ->
                       highLevelTokens
                          (toks:others) ReadingStringInCode
                          depth (factorize parserState tok lineAcc) acc
+                  CmtEnd mark ->
+                      error ("got EndCmtMArk while ReadingCode: " ++ mark)
+                  EscapedChar esc ->
+                      error ("got EscapedChar while ReadingCode: " ++ esc)
             ReadingShortCmt ->
                 case tok of
                   StringBeginOrEnd _ ->
                       highLevelTokens
                          (toks:others) ReadingStringInShortCmt
                          depth (factorize parserState tok lineAcc) acc
+                  EscapedChar esc ->
+                      error ("got EscapedChar while ReadingShortCmt: " ++ esc)
                   _ ->
                       highLevelTokens
                          (toks:others) parserState
@@ -244,11 +313,17 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
                       highLevelTokens
                          (toks:others) ReadingStringInLongCmt
                          depth (factorize parserState tok lineAcc) acc
+                  EscapedChar esc ->
+                      error ("got EscapedChar while ReadingLongCmt: " ++ esc)
             ReadingStringInLongCmt ->
                 case tok of
                   StringBeginOrEnd _ ->
                       highLevelTokens
                          (toks:others) ReadingLongCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  EscapedChar _ ->
+                      highLevelTokens
+                         (toks:others) ReadingEscCharInStringInLongCmt
                          depth (factorize parserState tok lineAcc) acc
                   _ ->
                       highLevelTokens
@@ -260,6 +335,10 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
                       highLevelTokens
                          (toks:others) ReadingShortCmt
                          depth (factorize parserState tok lineAcc) acc
+                  EscapedChar _ ->
+                      highLevelTokens
+                         (toks:others) ReadingEscCharInStringInShortCmt
+                         depth (factorize parserState tok lineAcc) acc
                   _ ->
                       highLevelTokens
                          (toks:others) parserState
@@ -270,9 +349,43 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
                       highLevelTokens
                          (toks:others) ReadingCode
                          depth (factorize parserState tok lineAcc) acc
+                  EscapedChar _ ->
+                      highLevelTokens
+                         (toks:others) ReadingEscCharInStringInCode
+                         depth (factorize parserState tok lineAcc) acc
                   _ ->
                       highLevelTokens
                          (toks:others) parserState
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingEscCharInStringInCode ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingCode
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInCode
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingEscCharInStringInShortCmt ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingShortCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInShortCmt
+                         depth (factorize parserState tok lineAcc) acc
+            ReadingEscCharInStringInLongCmt ->
+                case tok of
+                  StringBeginOrEnd _ ->
+                      highLevelTokens
+                         (toks:others) ReadingLongCmt
+                         depth (factorize parserState tok lineAcc) acc
+                  _ ->
+                      highLevelTokens
+                         (toks:others) ReadingStringInLongCmt
                          depth (factorize parserState tok lineAcc) acc
     where
       factorize :: ParserState -> LowLevelToken -> [HighLevelToken]
@@ -281,14 +394,10 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
       factorize state elt lst@(x:xs) =
         let elt' = promote state elt in
         case (x, elt') of
-          (Code y, Code z) ->
-              (Code (y ++ z)):xs
-          (ShortCmt y, ShortCmt z) ->
-              (ShortCmt (y ++ z)):xs
-          (LongCmt y, LongCmt z) ->
-              (LongCmt (y ++ z)):xs
-          (StringInCode y, StringInCode z) ->
-              (StringInCode (y ++ z)):xs
+          (Code y, Code z) -> (Code (y ++ z)):xs
+          (ShortCmt y, ShortCmt z) -> (ShortCmt (y ++ z)):xs
+          (LongCmt y, LongCmt z) -> (LongCmt (y ++ z)):xs
+          (StringInCode y, StringInCode z) -> (StringInCode (y ++ z)):xs
           (StringInShortCmt y, StringInShortCmt z) ->
               (StringInShortCmt (y ++ z)):xs
           (StringInLongCmt y, StringInLongCmt z) ->
@@ -297,7 +406,7 @@ highLevelTokens (line:others) parserState depth lineAcc acc =
 
 rmCmtsWrapper :: String -> IO [[HighLevelToken]]
 rmCmtsWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""] ["\\"]
        let highLevelToks = highLevelTokens lowLevelToks ReadingCode 0 [] []
            codeOnly = removeComments highLevelToks
        return codeOnly
@@ -312,11 +421,11 @@ rmCmtsWrapper fileName =
 
 lowLevelTokenizeWrapper :: String -> IO [[LowLevelToken]]
 lowLevelTokenizeWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""] ["\\"]
        return lowLevelToks
 
 highLevelTokenizeWrapper :: String -> IO [[HighLevelToken]]
 highLevelTokenizeWrapper fileName =
-    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""]
+    do lowLevelToks <- tokenizeFile fileName [] [] ["--"] ["\""] ["\\"]
        let highLevelToks = highLevelTokens lowLevelToks ReadingCode 0 [] []
        return highLevelToks
