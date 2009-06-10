35483dfd95597d688b4c0dd26f8c6d9ffa29adf5
Francois BERENGER <francois.berenger.working@gmail.com>
2009-06-10 22:11:24 +0900

    * changed option names * removing comments removes completely the line if it becomes empty

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Data.List (intersperse)
 import System.Environment (getArgs)
 
 import CommentRemoval (hltToString)
+import CommentRemoval (isOnlyIndentationLine)
 import CommentRemoval (rmCmtsWrapper)
 import CommentRemoval (lowLevelTokenizeWrapper, highLevelTokenizeWrapper)
 
@@ -11,9 +12,9 @@ usage :: IO ()
 usage = (error . concat . (intersperse "\n"))
             ["usage"
             ," -h | --help"
-            ," -rc  file-name  -> remove comments"
-            ," -ltok file-name -> low level tokenization"
-            ," -htok file-name -> high level tokenization"]
+            ," -rc  file-name -> remove comments"
+            ," -llt file-name -> low level tokenization"
+            ," -hlt file-name -> high level tokenization"]
 
 main :: IO ()
 main =
@@ -24,12 +25,15 @@ main =
          x:xs ->
              if x == "-rc"
              then do res <- rmCmtsWrapper (head xs)
-                     let res' = map (map hltToString) res
-                     mapM_ putStrLn (map concat res')
-             else if x == "-ltok"
+                     let res' = filter (not . onlyIndentOrNull) res
+                         res'' = map (map hltToString) res'
+                     mapM_ putStrLn (map concat res'')
+             else if x == "-llt"
              then do res <- lowLevelTokenizeWrapper (head xs)
                      mapM_ putStrLn (map show res)
-             else if x == "-htok"
+             else if x == "-hlt"
              then do res <- highLevelTokenizeWrapper (head xs)
                      mapM_ putStrLn (map show res)
              else usage
+    where
+      onlyIndentOrNull x = null x || isOnlyIndentationLine x
