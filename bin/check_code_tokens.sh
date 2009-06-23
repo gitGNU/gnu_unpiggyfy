b51defee7a6ee709b83bc03c37db63431fc8582d
Francois BERENGER <francois.berenger.working@gmail.com>
2009-06-23 13:57:52 +0900

    optimized

--- bin/check_code_tokens.sh
+++ bin/check_code_tokens.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-sed "s/^\[//g" | sed "s/\]$//g" | sed "s/\(Keyword \"\)/\\n\1/g" | sed "s/\(Spacing \"\)/\\n\1/g" | sed "s/\(VarOrFunOrConst \"\)/\\n\1/g" | sed "s/,$//g"
+sed -e "s/^\[//g" -e "s/\]$//g" -e "s/\(Keyword \"\)/\\n\1/g" -e "s/\(Spacing \"\)/\\n\1/g" -e "s/\(VarOrFunOrConst \"\)/\\n\1/g"| sed "s/,$//g"
