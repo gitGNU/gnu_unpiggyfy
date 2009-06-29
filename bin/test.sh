e45696f272df4ad01d07b4474fc8935d2fa1636e
Francois BERENGER <francois.berenger.working@gmail.com>
2009-06-29 18:16:33 +0900

    hardened diff

--- bin/test.sh
+++ bin/test.sh
@@ -14,7 +14,7 @@ test_01_diff=tmp/CommentRemoval.test_diff
 echo "testing "$test"..."
 
 bin/unpig -llt $test_01_in > $test_01_out
-diff $test_01_out $test_01_ref > $test_01_diff
+diff $test_01_out $test_01_ref > $test_01_diff 2>&1
 
 if [ `wc -c $test_01_diff | awk '{print $1}'` -ne 0 ] ; then
     echo "###"$test" ERRORS in diff "$test_01_out" "$test_01_ref
@@ -34,7 +34,7 @@ test_02_diff=tmp/CommentRemoval.test_diff
 echo "testing "$test"..."
 
 bin/unpig -hlt $test_02_in > $test_02_out
-diff $test_02_out $test_02_ref > $test_02_diff
+diff $test_02_out $test_02_ref > $test_02_diff 2>&1
 
 if [ `wc -c $test_02_diff | awk '{print $1}'` -ne 0 ] ; then
     echo "###"$test" ERRORS in diff "$test_02_out" "$test_02_ref
@@ -54,7 +54,7 @@ test_04_diff=tmp/CommentRemoval.test_diff
 echo "testing "$test"..."
 
 bin/unpig -tc $test_04_in > $test_04_out
-diff $test_04_out $test_04_ref > $test_04_diff
+diff $test_04_out $test_04_ref > $test_04_diff 2>&1
 
 if [ `wc -c $test_04_diff | awk '{print $1}'` -ne 0 ] ; then
     echo "###"$test" ERRORS in diff "$test_04_out" "$test_04_ref
@@ -74,7 +74,7 @@ test_03_diff=tmp/CommentRemoval.test_diff
 echo "testing "$test"..."
 
 bin/unpig -rc $test_03_in > $test_03_out
-diff $test_03_out $test_03_ref > $test_03_diff
+diff $test_03_out $test_03_ref > $test_03_diff 2>&1
 
 if [ `wc -c $test_03_diff | awk '{print $1}'` -ne 0 ] ; then
     echo "###"$test" ERRORS in diff "$test_03_out" "$test_03_ref
