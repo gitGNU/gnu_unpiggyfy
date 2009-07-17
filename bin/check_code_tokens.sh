0f2b3705a0b9ba2cde3b2558a85af48624d9c230
Francois BERENGER <francois.berenger.working@gmail.com>
2009-07-17 20:52:21 +0900

    updated to manage all the high level tokens

--- bin/check_code_tokens.sh
+++ bin/check_code_tokens.sh
@@ -2,4 +2,12 @@
 
 #set -x
 
-sed -e "s/^\[//g" -e "s/\]$//g" -e "s/\(Keyword \"\)/\\n\1/g" -e "s/\(Spacing \"\)/\\n\1/g" -e "s/\(VarOrFunOrConst \"\)/\\n\1/g"| sed "s/,$//g"
+# usage: cat [FILE] | check_code_tokens.sh
+
+sed -e "s/^\[//g" \
+    -e "s/\]$//g" \
+    -e "s/\(\(Special\)*Keyword \"\)/\\n\1/g" \
+    -e "s/\(Spacing \"\)/\\n\1/g" \
+    -e "s/\(VarOrFunOrConst \"\)/\\n\1/g" \
+    -e "s/\(ShortComment \"\)/\\n\1/g" \
+    -e "s/\(LongComment \"\)/\\n\1/g" | sed "s/,$//g"
